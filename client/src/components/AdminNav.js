c71aa887a923fbe8a5462597c171d7cd217b776e
shakilkhan12 <shakilkhan621@gmail.com>
2022-02-10 10:56:29 +0500

    logout admin

--- client/src/components/AdminNav.js
+++ client/src/components/AdminNav.js
@@ -1,10 +1,15 @@
-import {Link} from "react-router-dom"
+import {useDispatch} from "react-redux"
+import { logout } from "../store/reducers/authReducer";
 const AdminNav = ({openSidebar}) => {
+    const dispatch = useDispatch();
+    const adminLogout = () => {
+        dispatch(logout());
+    }
     return(
      <nav className="fixed left-0 sm:left-64 top-4 right-0 mx-4">
       <div className="bg-gray-800 w-full flex justify-between sm:justify-end items-center p-4">
       <i class="bi bi-filter-left text-white text-2xl cursor-pointer sm:hidden block" onClick={openSidebar}></i>
-       <Link to="/" className="py-2 px-4 bg-indigo-600 text-white rounded-md capitalize">logout</Link>
+       <button className="py-2 px-4 bg-indigo-600 text-white rounded-md capitalize" onClick={adminLogout}>logout</button>
       </div>
      </nav>
     )
--- client/src/store/reducers/authReducer.js
+++ client/src/store/reducers/authReducer.js
@@ -1,15 +1,34 @@
 import { createSlice } from "@reduxjs/toolkit";
+import jwtDecode from "jwt-decode"
 const adminStorage = localStorage.getItem('admin-token');
+function verifyToken() {
+    if(adminStorage) {
+       const decodeToken = jwtDecode(adminStorage);
+       const expiresIn = new Date(decodeToken.exp * 1000);
+       if(new Date() > expiresIn) {
+           localStorage.removeItem('admin-token');
+           return null;
+       } else {
+           return adminStorage;
+       }
+    } else {
+        return null;
+    }
+}
 const authReducer = createSlice({
     name: 'authReducer',
     initialState: {
-        adminToken: adminStorage ? adminStorage : null
+        adminToken: verifyToken()
     },
     reducers: {
         setAdminToken: (state, action) => {
           state.adminToken = action.payload;
+        },
+        logout: (state) => {
+          localStorage.removeItem('admin-token')
+          state.adminToken = null;
         }
     }
 })
-export const {setAdminToken} = authReducer.actions;
+export const {setAdminToken, logout} = authReducer.actions;
 export default authReducer.reducer;
