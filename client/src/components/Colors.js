6af78b257ed3864271470279db2a00643a245292
shakilkhan12 <shakilkhan621@gmail.com>
2022-03-28 09:38:28 +0500

    product colors

---
+++ client/src/components/Colors.js
@@ -0,0 +1,14 @@
+const Colors = ({colors, deleteColor}) => {
+    return (
+        <div>
+            {colors.length > 0 && <h1 className="capitalize mb-2 text-gray-400 text-base">colors list</h1>}
+            {colors.length > 0 && <div className="flex flex-wrap -mx-1">
+                {colors.map(color => (
+                    <div className="p-1" key={color.id}>
+                    <div className="w-[30px] h-[30px] rounded-full cursor-pointer" style={{background: color.color}} onClick={() => deleteColor(color)}></div></div>
+                ))}
+                </div>}
+        </div>
+    )
+}
+export default Colors;
--- client/src/screens/dashboard/CreateProduct.js
+++ client/src/screens/dashboard/CreateProduct.js
@@ -1,11 +1,33 @@
+import {useState} from "react";
 import { Link } from "react-router-dom"
+import {TwitterPicker} from "react-color"
+import { v4 as uuidv4 } from 'uuid';
 import ScreenHeader from "../../components/ScreenHeader"
 import Wrapper from "./Wrapper"
 import { useAllCategoriesQuery } from "../../store/services/categoryService"
 import Spinner from "../../components/Spinner"
+import Colors from "../../components/Colors";
 const CreateProduct = () => {
     const {data = [], isFetching} = useAllCategoriesQuery();
-    console.log(data, isFetching)
+    const [state, setState] = useState({
+        title: '',
+        price: 0,
+        discount: 0,
+        stock: 0,
+        category: '',
+        colors: []
+    });
+    const handleInput = e => {
+        setState({...state, [e.target.name]: e.target.value})
+    }
+    const saveColors = (color) => {
+        const filtered = state.colors.filter((clr) => clr.color !== color.hex);
+        setState({...state, colors: [...filtered, {color: color.hex, id: uuidv4()}]})
+    }
+    const deleteColor = color => {
+        const filtered = state.colors.filter(clr => clr.color !== color.color);
+        setState({...state, colors: filtered});
+    }
     return(
         <Wrapper>
             <ScreenHeader>
@@ -16,35 +38,37 @@ const CreateProduct = () => {
                     <div className="flex flex-wrap">
                         <div className="w-full md:w-6/12 p-3">
                             <label htmlFor="title" className="label">title</label>
-                            <input type="text" name="title" className="form-control" id="title" placeholder="title..." />
+                            <input type="text" name="title" className="form-control" id="title" placeholder="title..." onChange={handleInput} value={state.title} />
                         </div>
                         <div className="w-full md:w-6/12 p-3">
                             <label htmlFor="price" className="label">price</label>
-                            <input type="number" name="price" className="form-control" id="price" placeholder="price..." />
+                            <input type="number" name="price" className="form-control" id="price" placeholder="price..." onChange={handleInput} value={state.price} />
                         </div>
                         <div className="w-full md:w-6/12 p-3">
                             <label htmlFor="discount" className="label">discount</label>
-                            <input type="number" name="discount" className="form-control" id="discount" placeholder="discount..." />
+                            <input type="number" name="discount" className="form-control" id="discount" placeholder="discount..." onChange={handleInput} value={state.discount} />
                         </div>
                         <div className="w-full md:w-6/12 p-3">
                             <label htmlFor="stock" className="label">stock</label>
-                            <input type="number" name="stock" className="form-control" id="stock" placeholder="stock..." />
+                            <input type="number" name="stock" className="form-control" id="stock" placeholder="stock..." onChange={handleInput} value={state.stock} />
                         </div>
                         <div className="w-full md:w-6/12 p-3">
                             <label htmlFor="categories" className="label">categories</label>
-                            {!isFetching ? data?.categories?.length > 0 && <select name="categories" id="categories" className="form-control">
+                            {!isFetching ? data?.categories?.length > 0 && <select name="category" id="categories" className="form-control" onChange={handleInput} value={state.category}>
                                 <option value="">Choose category</option>
                                 {data?.categories?.map(category => (
                                     <option value={category.name} key={category._id}>{category.name}</option>
                                 ))}
                             </select> : <Spinner />}
-                            
-                           
                         </div>
+                        <div className="w-full md:w-6/12 p-3">
+                            <label htmlFor="colors" className="label">choose colors</label>
+                            <TwitterPicker onChangeComplete={saveColors} />
+                            </div>
                     </div>
                 </div>
                 <div className="w-full xl:w-4/12 p-3">
-                    colors and images
+                   <Colors colors={state.colors} deleteColor={deleteColor} />
                 </div>
             </div>
         </Wrapper>
