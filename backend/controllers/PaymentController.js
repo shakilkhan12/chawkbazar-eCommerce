c04c6c46d0fd16a50cf05d50e1da3cdd8f97a07a
shakilkhan12 <shakilkhan621@gmail.com>
2022-09-11 10:47:04 +0500

    order data and metadata

--- backend/controllers/PaymentController.js
+++ backend/controllers/PaymentController.js
@@ -1,17 +1,30 @@
 const stripe = require("stripe")(process.env.STRIPE_KEY);
 const User = require("../models/User");
 class PaymentController {
-  async paymentProcess(req, res) {
+  async paymentProcess(req, res, next) {
     const { cart, id } = req.body;
     const user = await User.findOne({ _id: id });
     if (!user) {
       return res.status(404).json({ error: "User not found" });
     }
+    const orderData = cart.map((item) => {
+      return {
+        _id: item._id,
+        size: item.size,
+        color: item.color,
+        quantity: item.quantity,
+      };
+    });
+    const customer = await stripe.customers.create({
+      email: user.email,
+      metadata: {
+        cart: JSON.stringify(orderData),
+      },
+    });
     const session = await stripe.checkout.sessions.create({
       shipping_address_collection: {
         allowed_countries: ["PK", "IN", "BD"],
       },
-      customer_email: user.email,
       shipping_options: [
         {
           shipping_rate_data: {
@@ -52,13 +65,47 @@ class PaymentController {
           quantity: item.quantity,
         };
       }),
-
+      customer: customer.id,
       mode: "payment",
-      success_url: `${process.env.CLIENT}/user`,
+      success_url: `${process.env.CLIENT}/user?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${process.env.CLIENT}/cart`,
     });
-
     res.json({ url: session.url });
+  }
+  async checkOutSession(request, response) {
+    const sig = request.headers["stripe-signature"];
+
+    let event;
+    try {
+      event = stripe.webhooks.constructEvent(
+        request.rawBody,
+        sig,
+        process.env.ENDPOINTSECRET
+      );
+    } catch (err) {
+      console.log(err.message);
+      response.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+
+    // Handle the event
+    switch (event.type) {
+      case "payment_intent.succeeded":
+        const paymentIntent = event.data.object;
+
+        // Then define and call a function to handle the event payment_intent.succeeded
+        break;
+      case "checkout.session.completed":
+        const data = event.data.object;
+        const customer = await stripe.customers.retrieve(data.customer);
+        console.log("customer :", customer);
+        break;
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
+    // Return a 200 response to acknowledge receipt of the event
+    response.send();
   }
 }
 module.exports = new PaymentController();
--- client/src/screens/users/Dashboard.js
+++ client/src/screens/users/Dashboard.js
@@ -1,30 +1,33 @@
-import { useSelector } from "react-redux"
-import Nav from "../../components/home/Nav"
-import Header from "../../components/home/Header"
-import AccountList from "../../components/home/AccountList"
+import { useSelector } from "react-redux";
+import { useSearchParams } from "react-router-dom";
+import Nav from "../../components/home/Nav";
+import Header from "../../components/home/Header";
+import AccountList from "../../components/home/AccountList";
 const Dashboard = () => {
-  const { user } = useSelector( state => state.authReducer );
+  const { user } = useSelector((state) => state.authReducer);
+  const [params] = useSearchParams();
+  console.log(params.get("session_id"));
   return (
     <>
-    <Nav />
-    <div className="mt-[70px]">
-      <Header>
-        my account
-      </Header>
-      <div className="my-container mt-[40px]">
-        <div className="flex flex-wrap -mx-6">
-          <div className="w-full md:w-4/12 p-6">
-            <AccountList />
-          </div>
-          <div className="w-full md:w-8/12 p-6">
-            <h1 className="heading">name</h1>
-            <span className="block mt-3 capitalize font-medium text-sm">{user?.name}</span>
+      <Nav />
+      <div className="mt-[70px]">
+        <Header>my account</Header>
+        <div className="my-container mt-[40px]">
+          <div className="flex flex-wrap -mx-6">
+            <div className="w-full md:w-4/12 p-6">
+              <AccountList />
+            </div>
+            <div className="w-full md:w-8/12 p-6">
+              <h1 className="heading">name</h1>
+              <span className="block mt-3 capitalize font-medium text-sm">
+                {user?.name}
+              </span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
